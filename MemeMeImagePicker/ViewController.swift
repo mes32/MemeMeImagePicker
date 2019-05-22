10047ed9cc02ab9ba19c587f373f8bc39f4a7ec9
mes32 <stockman.mike@gmail.com>
2019-05-22 09:04:03 -0500

    Add photo button.

--- MemeMeImagePicker/ViewController.swift
+++ MemeMeImagePicker/ViewController.swift
@@ -12,32 +12,38 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
 UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
+
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        print("imagePickerControllerDidCancel()")
         dismiss(animated: true, completion: nil)
     }
     
     func imagePickerController(_ picker: UIImagePickerController,
                                didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        print("imagePickerController()")
         if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-//            imageView.contentMode = .scaleAspectFit
             imageView.image = pickedImage
         }
-        
         dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func pickAnImage(_ sender: Any) {
-        
+    @IBAction func pickAnImageFromAlbum(_ sender: Any) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
         present(imagePicker, animated: true, completion: nil)
     }
 }
